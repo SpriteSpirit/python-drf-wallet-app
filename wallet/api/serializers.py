26bfaff56dbaaf53269a3184cb2cca2d7c4d0133
Angelina Khalueva <khalueva.angelina@gmail.com>
2025-02-01 16:23:46 +0300

    Реализован сериализатор для модели кошелька

---
+++ wallet/api/serializers.py
@@ -0,0 +1,37 @@
+from decimal import Decimal
+from typing import Optional
+from wallet.models import Wallet
+from rest_framework import serializers
+
+
+class WalletSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор для модели кошелька.
+    """
+
+    class Meta:
+        """
+        Метаданные сериализатора.
+        """
+
+        model = Wallet
+        fields = ['wallet_id', 'user', 'balance']
+
+
+class WalletOperationSerializer(serializers.Serializer):
+    """
+    Сериализатор для валидации данных операции (operationType и amount).
+    """
+
+    operation_type = serializers.ChoiceField(choices=['DEPOSIT', 'WITHDRAW'])
+    amount = serializers.DecimalField(min_value=Decimal('0.01'), max_digits=10, decimal_places=2)
+
+
+    def validate_amount(self, value: float) -> Optional[float]:
+        """
+        Проверка суммы на положительное значение.
+        """
+
+        if value < 0:
+            raise serializers.ValidationError("Сумма должна быть положительной.")
+        return value
