9afe2fd5c54264d28226fc8207adf68fec2b4437
Angelina Khalueva <khalueva.angelina@gmail.com>
2025-02-01 16:21:34 +0300

    Внесены исправления в модель пользователя

--- users/models.py
+++ users/models.py
@@ -1,5 +1,8 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models import Model
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from django.contrib.auth.models import AbstractUser
 
 
 NULLABLE = {'null': True, 'blank': True}
@@ -13,7 +16,6 @@ class User(AbstractUser):
         email (EmailField): Адрес электронной почты. Уникальное поле.
         first_name (CharField): Имя пользователя.
         last_name (CharField): Фамилия пользователя. Может быть пустым.
-        wallet_id (CharField): ID кошелька пользователя.
     """
 
     username = None
@@ -21,14 +23,39 @@ class User(AbstractUser):
     email = models.EmailField(unique=True, verbose_name='Email')
     first_name = models.CharField(max_length=100, verbose_name='Имя')
     last_name = models.CharField(max_length=100, verbose_name='Фамилия', **NULLABLE)
-    wallet_id = models.CharField(max_length=50, verbose_name='Wallet ID')
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
     class Meta:
+        """
+         Метаданные для определения наименований модели и её поведения.
+        """
+
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
 
-    def __str__(self):
-        return self.email
+    def __str__(self) -> str:
+        """
+        Возвращает строковое представление модели.
+        """
+
+        return f"{self.email}"
+
+
+@receiver(post_save, sender=User)
+def create_wallet(sender: type[Model], instance: User, created: bool, **kwargs) -> None:
+    """
+    При создании нового пользователя создает для него кошелек.
+
+    Args:
+        sender (type[Model]): Класс модели, который вызвал сигнал.
+        instance (User): Созданный или измененный экземпляр модели.
+        created (bool): Был ли создан новый экземпляр.
+        kwargs (dict): Дополнительные параметры.
+    """
+
+    from wallet.models import Wallet
+
+    if created:
+        Wallet.objects.create(user=instance, balance=0.0)
