7b98606684f63ee5595a35868d3076b252272da5
Angelina Khalueva <khalueva.angelina@gmail.com>
2025-02-03 02:47:10 +0300

    Добавлено логирование

--- users/tests.py
+++ users/tests.py
@@ -2,6 +2,7 @@ from rest_framework import status
 from rest_framework.test import APIClient, APITestCase
 
 from users.factories import UserFactory
+from utilities.logger_utils import logger
 
 
 class UserViewSetTestCase(APITestCase):
@@ -14,6 +15,8 @@ class UserViewSetTestCase(APITestCase):
         Тестирует создание нового пользователя.
         """
 
+        logger.info("Начало теста test_create_user")
+
         user_data = {
             'first_name': 'Test',
             'last_name': 'User',
@@ -21,25 +24,34 @@ class UserViewSetTestCase(APITestCase):
             'password': 'testpassword'
         }
 
+        logger.debug(f"Отправка запроса на создание пользователя с данными: {user_data}")
         response = self.client.post('/api/v1/users/', user_data, format='json')
+        logger.debug(f"Получен ответ от сервера: {response.content}")
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        logger.info("Тест test_create_user пройден успешно")
 
     def test_create_user_with_factory(self):
         """
         Тестирует создание нового пользователя с помощью фабрики.
         """
 
+        logger.info("Начало теста test_create_user_with_factory")
         user = UserFactory()
 
+        logger.debug(f"Создан пользователь с id: {user.id} и email: {user.email}")
         self.assertIsNotNone(user.id)
         self.assertRegex(user.email, r'^user\d+@example\.com$')
+        logger.info("Тест test_create_user_with_factory пройден успешно")
 
     def test_create_user_with_custom_email(self):
         """
         Тестирует создание нового пользователя с кастомным email.
         """
 
+        logger.info("Начало теста test_create_user_with_custom_email")
         user = UserFactory(email='custom@example.com')
 
+        logger.debug(f"Создан пользователь с id: {user.id} и email: {user.email}")
         self.assertIsNotNone(user.id)
         self.assertEqual(user.email, 'custom@example.com')
+        logger.info("Тест test_create_user_with_custom_email пройден успешно")
--- wallet/tests.py
+++ wallet/tests.py
@@ -6,6 +6,7 @@ from rest_framework.test import APITestCase  #, APIClient
 
 from users.factories import UserFactory
 from users.models import create_wallet, User
+from utilities.logger_utils import logger
 
 from wallet.factories import WalletFactory
 from wallet.services import perform_operation
@@ -49,56 +50,83 @@ class WalletServiceTests(APITestCase):
         Тестирует, что при создании второго кошелька для того же пользователя возникает ошибка IntegrityError.
         """
 
+        logger.info("Начало теста test_unique_wallet_per_user")
         user = UserFactory()
+        logger.debug(f"Создан пользователь с id: {user.id}")
         WalletFactory(user=user, balance=100.00)
+        logger.debug(f"Создан кошелек для пользователя с id: {user.id}")
 
         with self.assertRaises(IntegrityError):
+            logger.debug("Попытка создать второй кошелек для пользователя")
             WalletFactory(user=user, balance=50.00)
+        logger.info("Тест test_unique_wallet_per_user пройден успешно")
 
     def test_deposit(self):
         """
         Тестирует операцию снятия средств с кошелька.
         """
 
+        logger.info("Начало теста test_deposit")
         user = UserFactory()
+        logger.debug(f"Создан пользователь с id: {user.id}")
         self.wallet = WalletFactory(user=user, balance=100.00)
+        logger.debug(f"Создан кошелек для пользователя с id: {user.id} и балансом: {self.wallet.balance}")
 
+        logger.debug("Выполняется операция DEPOSIT")
         perform_operation(self.wallet.wallet_id, "DEPOSIT", Decimal(50.00))
 
         self.wallet.refresh_from_db()
+        logger.debug(f"Обновлен баланс кошелька: {self.wallet.balance}")
         self.assertEqual(self.wallet.balance, 150.00)
+        logger.info("Тест test_deposit пройден успешно")
 
     def test_withdraw(self):
         """
         Тестирует случай, когда попытка снятия средств превышает доступный баланс.
         """
 
+        logger.info("Начало теста test_withdraw")
         user = UserFactory()
+        logger.debug(f"Создан пользователь с id: {user.id}")
         self.wallet = WalletFactory(user=user, balance=100.00)
+        logger.debug(f"Создан кошелек для пользователя с id: {user.id} и балансом: {self.wallet.balance}")
 
+        logger.debug("Выполняется операция WITHDRAW")
         perform_operation(self.wallet.wallet_id, "WITHDRAW", Decimal(50.00))
 
         self.wallet.refresh_from_db()
+        logger.debug(f"Обновлен баланс кошелька: {self.wallet.balance}")
         self.assertEqual(self.wallet.balance, 50.00)
+        logger.info("Тест test_withdraw пройден успешно")
 
     def test_insufficient_funds(self):
         """
         Тестирует случай, когда указывается недопустимый тип операции.
         """
 
+        logger.info("Начало теста test_insufficient_funds")
         user = UserFactory()
+        logger.debug(f"Создан пользователь с id: {user.id}")
         self.wallet = WalletFactory(user=user, balance=100.00)
+        logger.debug(f"Создан кошелек для пользователя с id: {user.id} и балансом: {self.wallet.balance}")
 
+        logger.debug("Попытка выполнить операцию WITHDRAW с превышением баланса")
         with self.assertRaises(ValueError):
             perform_operation(self.wallet.wallet_id, "WITHDRAW", Decimal(150.00))
+        logger.info("Тест test_insufficient_funds пройден успешно")
 
     def test_invalid_operation_type(self):
         """
         Тестирует случай, когда указывается недопустимый тип операции.
         """
 
+        logger.info("Начало теста test_invalid_operation_type")
         user = UserFactory()
+        logger.debug(f"Создан пользователь с id: {user.id}")
         self.wallet = WalletFactory(user=user, balance=100.00)
+        logger.debug(f"Создан кошелек для пользователя с id: {user.id} и балансом: {self.wallet.balance}")
 
+        logger.debug("Попытка выполнить операцию с недопустимым типом")
         with self.assertRaises(ValueError):
             perform_operation(self.wallet.wallet_id, "INVALID", Decimal(50.00))
+        logger.info("Тест test_invalid_operation_type пройден успешно")
